d59a9437725c0caafff25345fc80f0249511d62d
Maikol1804 <maikbogi@gmail.com>
2014-04-20 22:45:50 -0500

    He agregado las librerias de akka y he colocado el codigo eje,plo del profe, falta mirar que es lo que toca hacer

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,38 @@
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
 
 public class Main {
+	/**
+	 * @mabonillagi tercer commit
+	 */
 
 	public static void main(String[] args) {
-		
-		// Primer commit con Git
-
+		ActorSystem system = ActorSystem.create("Hola");
+		ActorRef helloActor = system.actorOf(new Props(null, HelloActor.class,null), "hola"); //esta linea esta diferente
+		helloActor.tell("hello", null);
+		helloActor.tell("chao", null);
+		system.shutdown();
+		system.awaitTermination();// JOIN
 	}
+}
 
+class HelloActor extends UntypedActor {
+	@Override
+	public void onReceive(Object message) throws Exception {
+		if (message instanceof String) {
+			String s = (String) message;
+			switch (s) {
+			case "hello": {
+				System.out.println("hello you");
+				break;
+			}
+			default:
+				System.out.println("huu?");
+			}
+		} else {
+			unhandled(message);
+		}
+	}
 }
